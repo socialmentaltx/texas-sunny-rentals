c90e3d49194f27a487c234575891ffd048ba1e86
Dach <Socialmentaltx@gmail.com>
2026-08-21 22:05:54 -0500

    fix: add prisma generate postinstall, relax noImplicitAny for build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
 function createPrismaClient() {
   const connectionString = process.env.DATABASE_URL;
@@ -11,6 +12,6 @@ function createPrismaClient() {
   return new PrismaClient({ adapter });
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = global.prisma ?? createPrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
