1d26a200ce680aa8e802d710df3e379489da8edf
Dach <Socialmentaltx@gmail.com>
2026-08-21 22:14:14 -0500

    fix: downgrade to Prisma 5, remove v7 config, clean build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL;
-  const adapter = new PrismaPg({ connectionString });
-  return new PrismaClient({ adapter });
-}
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+  });
 
-export const prisma = global.prisma ?? createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
